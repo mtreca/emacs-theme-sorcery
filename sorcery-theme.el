fb041d0d8d4ae2840ddcfb1bd56fefe687da8c52
vxid <root@vxid.dev>
2020-04-14 00:42:38 +0200

    Fix theme load-path.

--- sorcery-theme.el
+++ sorcery-theme.el
@@ -903,7 +903,13 @@
                               `(pdf-view-midnight-colors '(,fg . ,bg))))
 
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'sorcery)
-(provide 'sorcery-theme)
 
 ;;; sorcery-theme.el ends here
